4368cb9d948786c7f0035684ddf219cea33f42ca
nathanprkr <parkern2@carleton.edu>
2024-06-26 13:14:29 -0500

    its broken still

--- sin.py
+++ sin.py
@@ -1,12 +1,11 @@
 import tensorflow as tf
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense
-import matplotlib.pyplot as plt
 import pandas as pd
 
 # import data
-training = pd.read_excel('sinData.xlsx')
-testing = pd.read_excel('sinTesting.xlsx')
+training = pd.read_excel('training.xlsx')
+testing = pd.read_excel('testing.xlsx')
 
 train_x = training['input']
 train_y = training['output']
@@ -15,7 +14,7 @@ test_y = testing['output']
 
 # Define the model
 model = Sequential()
-model.add(Dense(50, input_dim=1, activation='relu'))
+model.add(Dense(50, activation='relu'))
 model.add(Dense(50, activation='relu'))
 model.add(Dense(1))
 
@@ -23,15 +22,5 @@ model.add(Dense(1))
 model.compile(optimizer='adam', loss='mean_squared_error')
 
 # Train the model
-model.fit(train_x, train_y, epochs=100, batch_size=32, verbose=1)
-
-# Make predictions
-pred_y = model.predict(test_x)
-
-# Plot the results
-plt.figure(figsize=(10, 6))
-plt.plot(test_x, test_y, label='True Function')
-plt.plot(test_x, pred_y, label='Neural Network Predictions')
-plt.legend()
-plt.show()
+model.fit(train_x, train_y, epochs=5, batch_size=32, verbose=2)
 
